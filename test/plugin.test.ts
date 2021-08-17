50b1c91ba4296730aec652e8e8acbdb86719c8d8
skarab42 <skarab@bluewin.ch>
2021-08-17 13:36:06 +0200

    test: should export entry metadata

--- test/plugin.test.ts
+++ test/plugin.test.ts
@@ -1,4 +1,4 @@
-import { rollup } from "rollup";
+import { OutputAsset, OutputChunk, rollup } from "rollup";
 import css from "../dist/index";
 import rimraf from "rimraf";
 import glob from "glob";
@@ -20,6 +20,11 @@ function expectTree(fixtures: string[], outputs: string[]) {
   outputs.forEach((output, index) => {
     expect(readFile(fixtures[index] as string)).toEqual(readFile(output));
   });
+}
+
+function isEntryChunk(info: OutputAsset | OutputChunk): info is OutputChunk {
+  info = info as OutputChunk;
+  return info.isEntry === true && info.facadeModuleId !== null;
 }
 
 describe("rollup-plugin-css-export", () => {
@@ -118,5 +123,53 @@ describe("rollup-plugin-css-export", () => {
     });
 
     expectTree(fixtures, outputs);
+  });
+
+  it("should export entry metadata", async () => {
+    const metaKey = "styles";
+    const fixtures: string[] = [
+      "test/fixtures/red.css",
+      "test/fixtures/blue.css",
+      "test/fixtures/lib/reset.css",
+    ];
+    const outputs: string[] = [
+      "test/temp/assets/red.css",
+      "test/temp/assets/blue.css",
+      "test/temp/assets/reset.css",
+    ];
+    const expectedMetadata = {
+      "blue.js": ["assets/reset.css", "assets/blue.css"],
+      "red.js": ["assets/reset.css", "assets/red.css"],
+      "purple.js": ["assets/reset.css", "assets/red.css", "assets/blue.css"],
+    };
+
+    const metadata: Record<string, string[] | null> = {};
+
+    const bundle = await rollup({
+      input: [
+        "test/fixtures/blue.js",
+        "test/fixtures/red.js",
+        "test/fixtures/purple.js",
+      ],
+      plugins: [
+        css({ metaKey }),
+        {
+          name: "your-plugin",
+          generateBundle(_, bundle) {
+            Object.entries(bundle).forEach(([id, entry]) => {
+              if (isEntryChunk(entry) && entry.facadeModuleId) {
+                const info = this.getModuleInfo(entry.facadeModuleId);
+                metadata[id] = info?.meta[metaKey];
+              }
+            });
+          },
+        },
+      ],
+    });
+
+    await bundle.write(defaultOutputOptions);
+
+    expectTree(fixtures, outputs);
+    expect(metadata).toStrictEqual(expectedMetadata);
   });
 });
